cd917b9168cab2747cdb8a4a52c01835dcbf3845
Kev133 <114579575+Kev133@users.noreply.github.com>
2022-09-30 11:29:38 +0200

    pridana optimalizacni funkce

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import array
 import scipy.signal
+import scipy.optimize
 import matplotlib
 from scipy import cluster
 with open("testovaci_data.json", "r") as f:
@@ -11,26 +13,28 @@ with open("testovaci_data.json", "r") as f:
 #vypsal jsem si do seznamu "klice" klice ve slovniku a pak na ne odkazuji ne slovem ale cislem
 klice=list(dict.keys())
 #zkusebni data na porozumneni
-fakedata=[2,2,3,4,5,6]
-fakesonda=[1,2,3]
-sonda=dict[klice[0]]
+#fakedata=[2,2,3,4,5,6]
+#fakesonda=[1,2,3]
+sonda=array(dict[klice[0]])
 skut=dict[klice[1]]
 namer=dict[klice[2]]
-namer_sum=dict[klice[3]]
-nastrel=[1,1,1,1,1,1]
-konvoluce=list(np.convolve(fakesonda,fakedata,"same"))
-
+namer_sum=array(dict[klice[3]])
+konvoluce=list(np.convolve(sonda,namer_sum,"same"))
+print(len(konvoluce))
+x0=array(len(namer_sum)*[1])
+print(konvoluce)
 plt.plot(skut,marker="o",mfc="black")
 #plt.show()
-print(namer)
+def to_opt(x):
+   return array(np.convolve(x,sonda,"same"))-array(namer_sum)
+print(namer_sum)
 print(skut)
-#zkouska nástřelu, jenze jaka optimalizacni funkce?
-nastrel=[1,1,1,1,1,1]
-test1=list(np.convolve(fakesonda,nastrel))
-print(test1)
+heh=scipy.optimize.leastsq(to_opt,x0)
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
+print(heh)
 
 
-neco=scipy.signal.deconvolve(namer,sonda)
-print(neco)
 
 # print(json.dumps(dict,indent=4,sort_keys=True))
